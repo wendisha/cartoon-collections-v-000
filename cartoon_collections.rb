c85a0405d3f7abe0569a377dcd9950cc01d2f088
Wendy Calderon <wendycalderon@gmail.com>
2018-06-12 22:18:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ end
 def find_the_cheese (all_snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  all_snacks.detect?{|snack| cheese_types.include? snack}
+  all_snacks.detect{|snack| cheese_types.include? snack}
   #? true : nil
    
 
